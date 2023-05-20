6ca0ca2ea8cc3b9e9c46f514fe1ca13c6ddb36aa
Saif Helal <saif_abuelhasan@eng.suez.edu.eg>
2023-05-20 16:57:27 +0200

    Finished shop body validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import * as authController from "../controllers/authenticationController.js";
+import validateBody from "../middleware/validateBody.js";
+import * as authValidation from "../schemaValidations/authValidation.js";
 
 const router = express.Router();
 
-router.post("/login", authController.login);
+router.post("/login", validateBody(authValidation.login), authController.login);
 router.post("/signup", authController.signup);
 router.get("/logout", authController.logout);
 router.get("/google", authController.googleLogin);
