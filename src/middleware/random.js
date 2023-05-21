22dc4d3fb3973c220d57258d63b83f9b29b8e3f1
Saif Helal <saif_abuelhasan@eng.suez.edu.eg>
2023-05-21 18:03:21 +0200

    Fixed bug

--- src/middleware/random.js
+++ src/middleware/random.js
@@ -1,18 +1,18 @@
-// declare all characters
-const characters =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// // declare all characters
+// const characters =
+//   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-function Random(length) {
-  /**
-   * Generate a random string, acts as a shorter version of uuid
-   */
-  let result = '';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
+// function Random(length) {
+//   /**
+//    * Generate a random string, acts as a shorter version of uuid
+//    */
+//   let result = '';
+//   const charactersLength = characters.length;
+//   for (let i = 0; i < length; i++) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-export default Random;
+// export default Random;
